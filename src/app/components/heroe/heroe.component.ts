40c16d6477338e650e102233ec6396c5074e4196
zentaker <46875264+zentaker@users.noreply.github.com>
2019-07-05 19:23:52 -0500

    Add files via upload

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,33 @@
+import { Component  } from '@angular/core';
+import { ActivatedRoute  } from '@angular/router';
+import { HeroesService  } from '../../servicios/heroes.service';
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  
+})
+export class HeroeComponent  {
+
+  heroe: any = {};
+
+  constructor(private activateRoute:ActivatedRoute,
+              private _heroesService: HeroesService 
+    ) {
+
+    this.activateRoute.params.subscribe( params => {
+      
+      this.heroe = this._heroesService.getheroe(params['id']);
+      
+      console.log( this.heroe);
+    } )
+    
+
+
+
+   }
+
+ 
+
+}
